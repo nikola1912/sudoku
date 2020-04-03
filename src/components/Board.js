8b412d4238f005a0b16a0b9f43fa24cb4d8583ea
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-04-03 20:13:10 +0200

    Spliting squares and cells into their own components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,44 +1,59 @@
 import React from 'react';
 
-class Board extends React.Component {
-    
-    render() {
-        return (
-            <div>
-                {createBoard(this.props.board, this.props.boardSize)}
-            </div>
-        )
-    }
-}
-
-const createBoard = (board, boardSize) => {
-    let squareSize = boardSize**(1/2);
+const Board = (props) => {
     let newBoard = [];
-    for (let i = 0; i < boardSize; i++) {
-        newBoard.push(<div 
-            key={`square${i}`} 
-            className="board-square"
-            style={{gridTemplate: `repeat(${squareSize}, 1fr) / repeat(${squareSize}, 1fr)`}}>     
-        </div>);
-    }
-    
     let startIndex = 0;
+    const boardSize = props.boardSize;
+    const squareSize = boardSize**(1/2);
+
     for (let i = 0; i < boardSize; i++) {
-        let squareRow = [];
-        for (let row = startIndex; row < startIndex + squareSize * boardSize; row += boardSize) {
-            for (let index = row; index < row + squareSize; index++) {
-                squareRow.push(<div key={`cell${index}`} className="board-cell">{board[index]}</div>);
-            }
-        }
-        newBoard[i] = React.cloneElement(newBoard[i], null, [...squareRow]);
+        newBoard.push(<Square
+            board={props.board}
+            boardSize={boardSize}
+            squareSize={squareSize}
+            startIndex={startIndex} 
+        />);
         startIndex += squareSize;
     }
     
-    return <div
-        className="board"
-        style={{gridTemplate: `repeat(${squareSize}, 1fr) / repeat(${squareSize}, 1fr)`}}>
-            {newBoard}
-    </div>;
-};
+    return (
+        <div 
+            className={"board"}
+            style={{gridTemplate: `repeat(${squareSize}, 1fr) / repeat(${squareSize}, 1fr)`}}>
+                {[...newBoard]}
+        </div>
+    )
+}
+
+
+const Square = (props) => {
+    let squareCells = [];
+    const { board, boardSize, squareSize, startIndex } = props;
+    const squareIndex = startIndex / squareSize;
+    const lastRow = startIndex + squareSize * boardSize;
+
+    for (let row = startIndex; row < lastRow; row += boardSize) 
+        for (let index = row; index < row + squareSize; index++) 
+            squareCells.push(<Cell value={board[index]} cellIndex={index}/>);
+
+    return (
+        <div 
+            key={squareIndex}
+            className="board-square"
+            style={{gridTemplate: `repeat(${squareSize}, 1fr) / repeat(${squareSize}, 1fr)`}}>
+                {[...squareCells]}
+        </div>
+    );
+}
+
+const Cell = (props) => {
+    return (
+        <div
+            key={props.cellIndex}
+            className="board-cell">
+                {props.value}
+        </div>
+    );
+}
 
 export default Board;
