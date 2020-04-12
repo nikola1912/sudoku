4550b6f03e54114a7df90e0370a5d500e9f42dcf
Ognjenov Komp <nikola.jov1912@gmail.com>
2020-04-12 21:51:39 +0200

    Transform 'Board' into a function component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
 
-class Board extends React.Component {
-
-    render() {
-        const { board, boardSize } = this.props;
-        const squareSize = boardSize**(1/2);
-
-        return (
-            <div 
-                className={"board"}
-                style={{gridTemplate: `repeat(${boardSize}, 1fr) / repeat(${boardSize}, 1fr)`}}>
-                    {board.map((row, rowIndex) => row.map((cellValue, columnIndex) => (
-                        <Cell
-                            key={rowIndex * boardSize + columnIndex}
-                            value={cellValue}
-                            borderClasses={"" +
-                                (columnIndex % squareSize === 0 ? "border-left " : "") +
-                                (rowIndex % squareSize === 0 ? "border-top " : "")} />
-                    )))}
-            </div>
-        );
-    }
-}
+const Board = ({board, boardSize}) => (
+    <div 
+        className={"board"}
+        style={{gridTemplate: `repeat(${boardSize}, 1fr) / repeat(${boardSize}, 1fr)`}}>
+            {board.map((row, rowIndex) => row.map((cellValue, columnIndex) => (
+                <Cell
+                    key={rowIndex * boardSize + columnIndex}
+                    value={cellValue}
+                    borderClasses={"" +
+                        (columnIndex % boardSize**(1/2) === 0 ? "border-left " : "") +
+                        (rowIndex % boardSize**(1/2) === 0 ? "border-top " : "")} />
+            )))}
+    </div>
+);
 
 const Cell = (props) => (
     <div className={`board-cell ${props.borderClasses}`}>
