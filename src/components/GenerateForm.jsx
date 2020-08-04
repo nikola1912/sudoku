e7a94517ea467fa80b28636ec831bc2549b3e6d5
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-08-04 23:17:41 +0200

    Adapt 'GenerateForm' to ESLint rules

--- src/components/GenerateForm.jsx
+++ src/components/GenerateForm.jsx
@@ -1,83 +1,75 @@
-import React from 'react';
-import '../styles/Form.css';
-import RadioFieldset from './RadioFieldset.jsx';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-class GenerateForm extends React.Component {
-    state = {
-        boardSize: "9",
-        difficulty: "easy"
+import '../styles/Form.css'
+import RadioFieldset from './RadioFieldset.jsx'
+
+const GenerateForm = ({ onSubmit, onCancel, visability }) => {
+  const [boardSize, setBoardSize] = useState('9')
+  const [difficulty, setDifficulty] = useState('easy')
+
+  const handleFormChange = event => {
+    const possibleChanges = {
+      boardSize: () => setBoardSize(event.target.value),
+      difficulty: () => setDifficulty(event.target.value)
     }
+    possibleChanges[event.target.name]()
+  }
 
-    handleBoardSizeChange(boardSize) {
-        this.setState({boardSize});
-    }
+  const handleFormSubmit = event => {
+    event.preventDefault()
+    console.log(`Board size: ${boardSize} | Difficulty: ${difficulty}`)
+    handleFormCancel()
+    onSubmit({ boardSize, difficulty })
+  }
 
-    handleDifficultyChange(difficulty) {
-        this.setState({difficulty});
-    }
+  const handleFormCancel = () => {
+    setBoardSize('9')
+    setDifficulty('easy')
+    onCancel()
+  }
 
-    handleFormChange(event) {
-        switch (event.target.name) {
-            case "boardSize":
-                this.handleBoardSizeChange(event.target.value);
-                break;
-            case "difficulty":
-                this.handleDifficultyChange(event.target.value);
-                break;
-            default:
-                break;
-        }
-    }
+  return (
+    <form
+      noValidate
+      onSubmit={event => handleFormSubmit(event)}
+      className={visability ? 'form form-generate' : 'hidden'}
+    >
+      <div className="radio-container">
+        <RadioFieldset
+          name={'boardSize'}
+          title={'Board Size:'}
+          formatID={value => `generate${value}`}
+          values={['9', '16']}
+          stateToCheck={boardSize}
+          onChange={event => handleFormChange(event)}
+        />
 
-    handleFormSubmit(event) {
-        event.preventDefault();
-        console.log(this.state);
-        this.handleFormCancel();
-        this.props.onSubmit(this.state);
-    }
+        <RadioFieldset
+          name={'difficulty'}
+          title={'Difficulty:'}
+          formatID={value => value}
+          values={['easy', 'medium', 'hard']}
+          stateToCheck={difficulty}
+          onChange={event => handleFormChange(event)}
+        />
+      </div>
 
-    handleFormCancel() {
-        this.setState({
-            boardSize: "9",
-            difficulty: "easy"
-        }, () => this.props.onCancel())
-    }
-
-    render() {
-        return (
-            <form noValidate
-                onSubmit={event => this.handleFormSubmit(event)}
-                className={this.props.visability ? "form form-generate" : "hidden"}>
-                <div className="radio-container">
-                    <RadioFieldset 
-                        name={"boardSize"}
-                        title={"Board Size:"}
-                        formatID={value => `generate${value}`}
-                        values={["9", "16"]}
-                        stateToCheck={this.state.boardSize} 
-                        onChange={event => this.handleFormChange(event)} />
-
-                    <RadioFieldset 
-                        name={"difficulty"}
-                        title={"Difficulty:"}
-                        formatID={value => value}
-                        values={["easy", "medium", "hard"]}
-                        stateToCheck={this.state.difficulty} 
-                        onChange={event => this.handleFormChange(event)} />
-                </div>
-
-                <input
-                    type="submit"
-                    value="Generate"
-                    className="button" />
-                <input
-                    type="button"
-                    value="Cancel"
-                    className="button"
-                    onClick={() => this.handleFormCancel()} />
-            </form>
-        )
-    }
+      <input type="submit" value="Generate" className="button" />
+      <input
+        type="button"
+        value="Cancel"
+        className="button"
+        onClick={() => handleFormCancel()}
+      />
+    </form>
+  )
 }
 
-export default GenerateForm;
+GenerateForm.propTypes = {
+  onSubmit: PropTypes.func,
+  onCancel: PropTypes.func,
+  visability: PropTypes.bool
+}
+
+export default GenerateForm
