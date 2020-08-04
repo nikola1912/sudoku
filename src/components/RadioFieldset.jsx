d671b3ee1616f148109b810c240c4774f909ce72
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-08-04 09:16:06 +0200

    Adapt 'RadioFieldset' component to lint rules

--- src/components/RadioFieldset.jsx
+++ src/components/RadioFieldset.jsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const RadioFieldset = (props) => (
-    <fieldset className={`radio-${props.name}`}>
-        <legend>
-            {props.questionMark && <span className="questionMark">?</span>}
-            {props.title}
-        </legend>
-        {props.values.map(value => 
-            <span key={value}>
-                <input 
-                    disabled={props.disabled && "disabled"}
-                    type="radio"
-                    name={props.name}
-                    id={props.formatID(value)}
-                    value={value}
-                    checked={props.stateToCheck === value}
-                    onChange={event => props.onChange(event)} />
-                <label htmlFor={props.formatID(value)}>
-                    {props.name === "boardSize" ?
-                        `${value}x${value}` :
-                        `${value[0].toUpperCase()}${value.slice(1)}`}
-                </label>
-            </span>
-        )}
+const RadioFieldset = ({
+  name,
+  questionMark,
+  title,
+  values,
+  disabled,
+  formatID,
+  stateToCheck,
+  onChange
+}) => (
+    <fieldset className={`radio-${name}`}>
+      <legend>
+        {questionMark && <span className="questionMark">?</span>}
+        {title}
+      </legend>
+      {values.map(value => (
+        <span key={value}>
+          <input
+            disabled={disabled && 'disabled'}
+            type="radio"
+            name={name}
+            id={formatID(value)}
+            value={value}
+            checked={stateToCheck === value}
+            onChange={event => onChange(event)}
+          />
+          <label htmlFor={formatID(value)}>
+            {name === 'boardSize'
+              ? `${value}x${value}`
+              : `${value[0].toUpperCase()}${value.slice(1)}`}
+          </label>
+        </span>
+      ))}
     </fieldset>
-)
+  )
 
-export default RadioFieldset;
+RadioFieldset.propTypes = {
+  name: PropTypes.string,
+  questionMark: PropTypes.bool,
+  title: PropTypes.string,
+  values: PropTypes.arrayOf(PropTypes.string),
+  disabled: PropTypes.bool,
+  formatID: PropTypes.func,
+  stateToCheck: PropTypes.string,
+  onChange: PropTypes.func
+}
+
+export default RadioFieldset
