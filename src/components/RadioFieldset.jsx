55998dfc1f903d9c1758111a12b7a14e0985e356
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-08-04 09:29:01 +0200

    Fix 'RadioFieldset' indentation

--- src/components/RadioFieldset.jsx
+++ src/components/RadioFieldset.jsx
@@ -11,31 +11,31 @@ const RadioFieldset = ({
   stateToCheck,
   onChange
 }) => (
-    <fieldset className={`radio-${name}`}>
-      <legend>
-        {questionMark && <span className="questionMark">?</span>}
-        {title}
-      </legend>
-      {values.map(value => (
-        <span key={value}>
-          <input
-            disabled={disabled && 'disabled'}
-            type="radio"
-            name={name}
-            id={formatID(value)}
-            value={value}
-            checked={stateToCheck === value}
-            onChange={event => onChange(event)}
-          />
-          <label htmlFor={formatID(value)}>
-            {name === 'boardSize'
-              ? `${value}x${value}`
-              : `${value[0].toUpperCase()}${value.slice(1)}`}
-          </label>
-        </span>
-      ))}
-    </fieldset>
-  )
+  <fieldset className={`radio-${name}`}>
+    <legend>
+      {questionMark && <span className="questionMark">?</span>}
+      {title}
+    </legend>
+    {values.map(value => (
+      <span key={value}>
+        <input
+          disabled={disabled && 'disabled'}
+          type="radio"
+          name={name}
+          id={formatID(value)}
+          value={value}
+          checked={stateToCheck === value}
+          onChange={event => onChange(event)}
+        />
+        <label htmlFor={formatID(value)}>
+          {name === 'boardSize'
+            ? `${value}x${value}`
+            : `${value[0].toUpperCase()}${value.slice(1)}`}
+        </label>
+      </span>
+    ))}
+  </fieldset>
+)
 
 RadioFieldset.propTypes = {
   name: PropTypes.string,
