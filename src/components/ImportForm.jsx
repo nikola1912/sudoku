abf04832010c8b0643d320dc12f1ab222c245d53
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-08-05 20:29:58 +0200

    Adapt 'ImportForm' to ESLint rules

--- src/components/ImportForm.jsx
+++ src/components/ImportForm.jsx
@@ -1,128 +1,164 @@
-import React from 'react';
-import '../styles/Form.css';
-import RadioFieldset from './RadioFieldset.jsx';
-import arrayToMatrix from '../util/arrayToMatrix.js';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-class ImportForm extends React.Component {
-    state = {
-        boardSize: "9",
-        inputMode: "",
-        inputCode: "",
-        validInputs: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-        showErrorMessage: false
+import '../styles/Form.css'
+import RadioFieldset from './RadioFieldset.jsx'
+import arrayToMatrix from '../util/arrayToMatrix.js'
+
+const ImportForm = ({ onSubmit, onCancel, visability }) => {
+  const [boardSize, setBoardSize] = useState('9')
+  const [inputMode, setInputMode] = useState('')
+  const [inputCode, setInputCode] = useState('')
+  const [showErrorMessage, setShowErrorMessage] = useState(false)
+  const [validInputs, setValidInputs] = useState([
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
+  ])
+
+  const handleInputModeChange = inputMode => setInputMode(inputMode)
+  const handleInputCodeChange = inputCode => setInputCode(inputCode)
+  const hideErrorMessage = () => setShowErrorMessage(false)
+  const handleBoardSizeChange = boardSize => {
+    const possibleBoardSizes = {
+      '9': ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      '16': [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F'
+      ]
     }
+    setValidInputs(possibleBoardSizes[boardSize])
+    setBoardSize(boardSize)
+  }
 
-    handleBoardSizeChange(boardSize) {
-        if (boardSize === "9")
-            this.setState({validInputs: ["1", "2", "3", "4", "5", "6", "7", "8", "9"]});
-        else if (boardSize === "16")
-            this.setState({validInputs: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]});
-        this.setState({boardSize});
-    }
+  const validateForm = () => {
+    const requiredCodeLength = parseInt(boardSize) ** 2
+    return inputCode.length === requiredCodeLength
+  }
 
-    handleInputModeChange(inputMode) {
-        this.setState({inputMode});
-    }
+  const formatInputCode = inputCode =>
+    inputCode.map(value =>
+      validInputs.includes(value.toUpperCase()) ? value.toUpperCase() : ' '
+    )
 
-    handleInputCodeChange(inputCode) {
-        this.setState({inputCode});
-    }
+  const handleFormSubmit = event => {
+    event.preventDefault()
+    if (validateForm()) {
+      handleFormCancel()
+      onSubmit({
+        board: arrayToMatrix(formatInputCode(Array.from(inputCode)), boardSize),
+        boardSize: Number(boardSize),
+        inputMode: inputMode
+      })
+    } else setShowErrorMessage(true)
+  }
 
-    hideErrorMessage() {
-        this.setState({showErrorMessage: false});
-    }
+  const handleFormCancel = () => {
+    setBoardSize('9')
+    setInputMode('')
+    setInputCode('')
+    setShowErrorMessage(false)
+    onCancel()
+  }
 
-    validateForm() {
-        const requiredCodeLength = parseInt(this.state.boardSize)**2;
-        return this.state.inputCode.length === requiredCodeLength;
-    }
+  return (
+    <form
+      noValidate
+      onChange={() => hideErrorMessage()}
+      onSubmit={event => handleFormSubmit(event)}
+      className={visability ? 'form form-import' : 'hidden'}
+    >
+      <div className="radio-container">
+        <RadioFieldset
+          name="boardSize"
+          title={'Board Size:'}
+          formatID={value => `${value}x${value}`}
+          values={['9', '16']}
+          stateToCheck={boardSize}
+          onChange={event => handleBoardSizeChange(event.target.value)}
+        />
 
-    formatInputCode(inputCode) {
-        return inputCode.map(value => this.state.validInputs.includes(value.toUpperCase()) ? value.toUpperCase() : " ");
-    }
+        <RadioFieldset
+          disabled={true}
+          questionMark={true}
+          name={'inputMode'}
+          title={'Input Mode:'}
+          formatID={value => value}
+          values={['row', 'square']}
+          stateToCheck={inputMode}
+          onChange={event => handleInputModeChange(event.target.value)}
+        />
+      </div>
 
-    handleFormSubmit(event) {
-        event.preventDefault();
-        if (this.validateForm()) {
-            this.handleFormCancel();
-            this.props.onSubmit({
-                board: arrayToMatrix(this.formatInputCode(Array.from(this.state.inputCode)), this.state.boardSize),
-                boardSize: Number(this.state.boardSize),
-                inputMode: this.state.inputMode
-            });
-        } else this.setState({showErrorMessage: true});
-    }
-    
-    handleFormCancel() {
-        this.setState({
-            boardSize: "9",
-            inputMode: "",
-            inputCode: "",
-            showErrorMessage: false
-        }, () => this.props.onCancel());
-    }
+      <div className="input-code-container">
+        <label>
+          <span className="questionMark">?</span>
+          Sudoku Code:
+        </label>
+        <input
+          required
+          type="text"
+          name="inputCode"
+          value={inputCode}
+          onChange={event => handleInputCodeChange(event.target.value)}
+        />
+        <ErrorMessage
+          className={`error-message ${
+            showErrorMessage ? 'show-error-message' : ''
+          }`}
+          inputValid={() => validateForm()}
+          requiredCodeLength={boardSize ** 2}
+        />
+      </div>
 
-    render() {
-        return (
-            <form noValidate
-                onChange={() => this.hideErrorMessage()}
-                onSubmit={event => this.handleFormSubmit(event)}
-                className={this.props.visability ? "form form-import" : "hidden"}>
-                <div className="radio-container">
-                    <RadioFieldset 
-                        name="boardSize"
-                        title={"Board Size:"}
-                        formatID={value => `${value}x${value}`}
-                        values={["9", "16"]}
-                        stateToCheck={this.state.boardSize} 
-                        onChange={event => this.handleBoardSizeChange(event.target.value)} />
-
-                    <RadioFieldset
-                        disabled={true}
-                        questionMark={true}
-                        name={"inputMode"}
-                        title={"Input Mode:"}
-                        formatID={value => value}
-                        values={["row", "square"]}
-                        stateToCheck={this.state.inputMode} 
-                        onChange={event => this.handleInputModeChange(event.target.value)} />
-                </div>
-
-                <div className="input-code-container">
-                    <label>
-                        <span className="questionMark">?</span>
-                        Sudoku Code:
-                    </label>
-                    <input required
-                        type="text"
-                        name="inputCode"
-                        value={this.state.inputCode}
-                        onChange={event => this.handleInputCodeChange(event.target.value)} />
-                    <ErrorMessage 
-                        className={`error-message ${this.state.showErrorMessage ? "show-error-message" : ""}`}
-                        inputValid={() => this.validateForm()}
-                        requiredCodeLength={this.state.boardSize**2} />
-                </div>
-
-                <input
-                    type="submit"
-                    value="Submit"
-                    className="button" />
-                <input
-                    type="button"
-                    value="Cancel"
-                    className="button"
-                    onClick={() => this.handleFormCancel()} />
-            </form>
-        )
-    }
+      <input type="submit" value="Submit" className="button" />
+      <input
+        type="button"
+        value="Cancel"
+        className="button"
+        onClick={() => handleFormCancel()}
+      />
+    </form>
+  )
 }
 
-const ErrorMessage = (props) => (
-    <span className={props.className}>
-        {props.inputValid ?
-        `Input code doesn't contain ${props.requiredCodeLength} values` : ""}
-    </span>
+const ErrorMessage = ({ className, inputValid, requiredCodeLength }) => (
+  <span className={className}>
+    {inputValid
+      ? `Input code doesn't contain ${requiredCodeLength} values`
+      : ''}
+  </span>
 )
 
-export default ImportForm;
+ImportForm.propTypes = {
+  onSubmit: PropTypes.func,
+  onCancel: PropTypes.func,
+  visability: PropTypes.bool
+}
+
+ErrorMessage.propTypes = {
+  className: PropTypes.string,
+  inputValid: PropTypes.bool,
+  requiredCodeLength: PropTypes.number
+}
+
+export default ImportForm
