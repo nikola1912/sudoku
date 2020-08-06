18c0e97ba3a46d67724e91433a5f5739f4cb8340
Nikola Jovanovic <nikola.jov1912@gmail.com>
2020-08-06 22:57:44 +0200

    Adapt 'App' component to ESLint rules

---
+++ src/components/App.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react'
+import '../styles/App.css'
+
+import Board from './Board.jsx'
+import ImportForm from './ImportForm.jsx'
+import ExportForm from './ExportForm.jsx'
+import GenerateForm from './GenerateForm.jsx'
+import { HeaderButtons, FooterButtons } from './Buttons.jsx'
+import sudokuSolver from '../util/sudokuSolver.js'
+
+/* const generateGrid = size => {
+  const grid = []
+  let index = 0
+  for (let i = 0; i < size; i++) {
+    const row = []
+    for (let j = 0; j < size; j++) {
+      row.push(index)
+      index++
+    }
+    grid.push(row)
+  }
+  return grid
+} */
+
+const App = () => {
+  const [boardSize, setBoardSize] = useState('9')
+  const [board, setBoard] = useState([
+    [' ', '3', ' ', ' ', ' ', ' ', ' ', '5', ' '],
+    [' ', ' ', '8', ' ', '9', '1', '3', ' ', ' '],
+    ['6', ' ', ' ', '4', ' ', ' ', '7', ' ', ' '],
+    [' ', ' ', '3', '8', '1', ' ', ' ', ' ', ' '],
+    [' ', ' ', '6', ' ', ' ', ' ', '2', ' ', ' '],
+    [' ', ' ', ' ', ' ', '3', '4', '8', ' ', ' '],
+    [' ', ' ', '1', ' ', ' ', '8', ' ', ' ', '9'],
+    [' ', ' ', '4', '1', '2', ' ', '6', ' ', ' '],
+    [' ', '6', ' ', ' ', ' ', ' ', ' ', '4', ' ']
+  ])
+  // const [board, setBoard] = useState(generateGrid(9))
+  const [buttonsVisability, setButtonsVisability] = useState(true)
+  const [importVisability, setImportVisability] = useState(false)
+  const [generateVisability, setGenerateVisability] = useState(false)
+  const [exportVisability, setExportVisability] = useState(false)
+  const [testMode] = useState(false)
+
+  const handleImportSubmit = ({ board, boardSize, inputMode }) => {
+    setBoard(board)
+    setBoardSize(boardSize)
+    // setInputMode(inputMode) TODO
+  }
+
+  const handleGenerateSubmit = generateData => {
+    console.log(generateData)
+  }
+
+  const handleCancel = () => {
+    setImportVisability(false)
+    setGenerateVisability(false)
+    setExportVisability(false)
+    setButtonsVisability(true)
+  }
+
+  const displayImportForm = () => {
+    setButtonsVisability(false)
+    setImportVisability(true)
+  }
+
+  const displayGenerateForm = () => {
+    setButtonsVisability(false)
+    setGenerateVisability(true)
+  }
+
+  const displayExportForm = () => {
+    setButtonsVisability(false)
+    setExportVisability(true)
+  }
+
+  const handleSolve = () => {
+    sudokuSolver(board)
+  }
+
+  return (
+    <div className="game">
+      <div className="header-Container">
+        <HeaderButtons
+          onSolve={() => handleSolve()}
+          // onReset={() => handleReset()} TODO
+        />
+      </div>
+
+      <div className="board-container">
+        <Board board={board} boardSize={boardSize} testMode={testMode} />
+      </div>
+
+      <div className="footer-container">
+        <FooterButtons
+          onImport={() => displayImportForm()}
+          onExport={() => displayExportForm()}
+          onGenerate={() => displayGenerateForm()}
+          visability={buttonsVisability}
+        />
+        <ImportForm
+          onSubmit={importData => handleImportSubmit(importData)}
+          onCancel={() => handleCancel()}
+          visability={importVisability}
+        />
+        <GenerateForm
+          onSubmit={generateData => handleGenerateSubmit(generateData)}
+          onCancel={() => handleCancel()}
+          visability={generateVisability}
+        />
+        <ExportForm
+          onCancel={() => handleCancel()}
+          visability={exportVisability}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App.jsx'
+import App from './components/App.jsx'
 import './styles/index.css'
 
 ReactDOM.render(
