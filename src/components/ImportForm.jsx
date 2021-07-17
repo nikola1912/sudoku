ec092cf17e072e7e034988297a25fc2d6bda1782
Nikola Jovanovic <nikola.jov1912@gmail.com>
2021-07-17 23:34:14 +0200

    Implement image upload prototype

--- src/components/ImportForm.jsx
+++ src/components/ImportForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { FiUpload } from 'react-icons/fi'
 
 import '../styles/Form.css'
 import RadioFieldset from './RadioFieldset.jsx'
@@ -8,11 +9,21 @@ import formatSudoku from '../util/formatSudoku.js'
 
 const ImportForm = ({ onSubmit, onCancel, visability }) => {
   const [boardSize, setBoardSize] = useState('9')
-  const [inputMode, setInputMode] = useState('')
+  const [inputMode, setInputMode] = useState('code')
   const [inputCode, setInputCode] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
 
-  const handleInputModeChange = inputMode => setInputMode(inputMode)
+  const handleInputModeChange = inputMode => {
+    setInputCode('')
+    setInputMode(inputMode)
+  }
+  const handleImageUpload = async image => {
+    const formData = new FormData()
+    formData.append('image', image)
+    const response = await fetch('/scanner', { method: 'POST', body: formData })
+    const data = await response.json()
+    console.log(data)
+  }
   const handleInputCodeChange = inputCode => setInputCode(inputCode)
   const handleBoardSizeChange = boardSize => setBoardSize(boardSize)
   const hideErrorMessage = () => setErrorMessage('')
@@ -29,7 +40,7 @@ const ImportForm = ({ onSubmit, onCancel, visability }) => {
 
   const handleFormCancel = () => {
     setBoardSize('9')
-    setInputMode('')
+    setInputMode('code')
     setInputCode('')
     setErrorMessage('')
     onCancel()
@@ -53,31 +64,53 @@ const ImportForm = ({ onSubmit, onCancel, visability }) => {
         />
 
         <RadioFieldset
-          disabled={true}
           questionMark={true}
           name={'inputMode'}
           title={'Input Mode:'}
           formatID={value => value}
-          values={['row', 'square']}
+          values={['code', 'image']}
           stateToCheck={inputMode}
           onChange={event => handleInputModeChange(event.target.value)}
         />
       </div>
 
-      <div className="input-code-container">
-        <label>
-          <span className="questionMark">?</span>
-          Sudoku Code:
-        </label>
-        <input
-          required
-          type="text"
-          name="inputCode"
-          value={inputCode}
-          onChange={event => handleInputCodeChange(event.target.value)}
-        />
-        <span className={'error-message'}>{errorMessage}</span>
-      </div>
+      {inputMode === 'code' && (
+        <div className="input-code-container">
+          <label>
+            <span className="questionMark">?</span>
+            Sudoku Code:
+          </label>
+          <input
+            required
+            type="text"
+            name="inputCode"
+            value={inputCode}
+            onChange={event => handleInputCodeChange(event.target.value)}
+          />
+          <span className={'error-message'}>{errorMessage}</span>
+        </div>
+      )}
+
+      {inputMode === 'image' && (
+        <div className="image-upload-container">
+          <span className="image-upload-label">
+            <span className="questionMark">?</span>Sudoku Scanner:
+          </span>
+          <label htmlFor="image-upload" className="image-upload-button">
+            <FiUpload /> <span>Upload</span>
+          </label>
+          <input
+            hidden="hidden"
+            id="image-upload"
+            name="inputImage"
+            type="file"
+            accept="image/*"
+            capture="camera"
+            onChange={event => handleImageUpload(event.target.files[0])}
+          />
+          <span className={'error-message'}>{errorMessage}</span>
+        </div>
+      )}
 
       <input type="submit" value="Submit" className="button" />
       <input
