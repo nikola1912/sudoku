14247e4fae2dfcab3f05b4c0042c60fec7111aef
Nikola Jovanovic <nikola.jov1912@gmail.com>
2021-07-26 23:47:42 +0200

    Create ErrorFallback component

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,9 @@ import { FC, Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import { initMocks } from '@/api'
-import { Button } from '@/components/Button'
+import { ErrorFallback } from '@/components/ErrorFallback'
 import { Spinner } from '@/components/Spinner'
 import { Sudoku } from '@/components/Sudoku'
-
-const ErrorFallback = () => {
-  return (
-    <div
-      className="text-red-500 w-screen h-screen flex flex-col justify-center items-center"
-      role="alert"
-    >
-      <h2 className="text-lg font-semibold">Ooops, something went wrong :( </h2>
-      <Button
-        className="mt-4"
-        onClick={() => window.location.assign(window.location.origin)}
-      >
-        Refresh
-      </Button>
-    </div>
-  )
-}
 
 initMocks()
 
