76c0a0120b0dd4ebe2c6058bc0a17a96aac714a9
Nikola Jovanovic <nikola.jov1912@gmail.com>
2021-08-08 15:05:37 +0200

    Update ErrorFallback component styling

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,16 +1,12 @@
 import { Button } from '@/components/Button'
 
+import styles from './styles.module.scss'
+
 const ErrorFallback = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center w-screen h-screen text-red-500"
-      role="alert"
-    >
-      <h2 className="text-lg font-semibold">Ooops, something went wrong :( </h2>
-      <Button
-        className="mt-4"
-        onClick={() => window.location.assign(window.location.origin)}
-      >
+    <div className={styles.errorFallback} role="alert">
+      <h2 className={styles.errorMessage}>Ooops, something went wrong :( </h2>
+      <Button onClick={() => window.location.assign(window.location.origin)}>
         Refresh
       </Button>
     </div>
